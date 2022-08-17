77091b5ffc65330f194562ffb6f4b708505578c2
Kaan <kaanmol@outlook.com>
2022-08-17 22:45:50 +0200

    Improved sync route

--- src/routes/api/sync.ts
+++ src/routes/api/sync.ts
@@ -9,11 +9,11 @@ export const GET: RequestHandler = async ({ locals }) => {
     const result = (await openProvider.dns.zone.list({
         with_records: true
     })).data.results;
-    
+
     console.log(result)
     for (const zone of result) {
         const tld = zone.name.split(".");
-        if (["nl", "com", "dev"].includes(tld[1]) === false) continue;
+        if (Object.values(DomainExtensions).includes(tld[1]) === false) continue;
         const domain = await prisma.domain.create({
             data: {
                 name: tld[0],
@@ -23,14 +23,18 @@ export const GET: RequestHandler = async ({ locals }) => {
 
         if (zone.records !== undefined) {
             for (const record of zone.records) {
-                if (["A", "AAAA", "CNAME"].includes(record.type) === false) continue;
-
+                console.log(record.type)
+                console.log(DnsRecordType[record.type as keyof typeof DnsRecordType])
                 await prisma.records.create({
                     data: {
                         name: record.name,
                         type: DnsRecordType[record.type as keyof typeof DnsRecordType],
                         value: record.value,
-                        domainId: domain.id,
+                        Domain: {
+                            connect: {
+                                id: domain.id
+                            }
+                        },
                         ttl: record.ttl
                     }
                 })
